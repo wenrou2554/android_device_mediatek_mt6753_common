daeec03ecff732ca8a78cc40a6eabb4cb3df8355
Moyster <oysterized@gmail.com>
2016-11-06 18:25:36 +0100

    add uninstall script for patches

---
+++ patches/uninstall.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+echo $1
+rootdirectory="$PWD"
+# ---------------------------------
+
+dirs="frameworks/av frameworks/base frameworks/opt/telephony hardware/libhardware hardware/libhardware_legacy packages/apps/Settings packages/services/telephony system/core system/netd"
+
+VENDOR=meizu
+DEVICE=m2note
+
+for dir in $dirs ; do
+	cd $rootdirectory
+	cd $dir
+	echo -e "Cleaning $dir patches...\n"
+	git reset --hard && git clean -df
+done
+
+# -----------------------------------
+echo -e "Done !\n"
+cd $rootdirectory
+
